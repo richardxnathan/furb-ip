a2c377485a01878e59e391df97b6d62d5da12db4
dalton-reis <dalton.reis@gmail.com>
2023-08-21 22:35:57 -0300

    .

--- Unidade3/src/Uni3Exe01.java
+++ Unidade3/src/Uni3Exe01.java
@@ -1,24 +1,65 @@
+/*
+
+__ Enunciado:
+Uma imobiliária vende apenas terrenos retangulares. Faça um programa para ler as
+dimensões de um terreno e depois exibir a área do terreno.
+
+__ Análise: OK
+
+__ Gordura: 
+
+Uma imobiliária vende apenas Faça um programa para 
+
+//__ Entradas: 
+
+ler(largura) ler(comprimento)
+
+//__ Processos: 
+
+area = largura x comprimento
+
+//__ Saídas: // área do terreno
+
+imprimir(area)
+
+*/
+
 import java.util.Scanner;
 
 public class Uni3Exe01 {
   public static void main(String[] args) {
-    // Dados
     Scanner teclado = new Scanner(System.in);
-    double altura;
+
+    // __ Variáveis:
     double largura;
-    double area;
+    double comprimento;
 
-    // Entrada
-    System.out.print("Altura: ");
-    altura = teclado.nextDouble();
-    System.out.print("Largura: ");
+    // __ Entradas:
+    System.out.print("Entre com a largura: ");
     largura = teclado.nextDouble();
+    System.out.print("Entre com a comprimento: ");
+    comprimento = teclado.nextDouble();
 
-    // Processo
-    area = altura * largura;
+    // __ Processos:
+    double area = largura * comprimento;
 
-    // Saída
-    System.out.print("Área do terreno: " + area);
+    // __ Saídas: área do terreno
+
+    System.out.println("A área do terreno: " + area);
+
     teclado.close();
   }
 }
+
+/*
+ * __ Valores de Testes:
+ * _ Caso 1 ___
+ * largura: 2
+ * comprimento: 3
+ * area: 6
+ * 
+ * _ Caso 2 ___
+ * largura: 3.7
+ * comprimento: 2.4
+ * area: 8.88
+ */
