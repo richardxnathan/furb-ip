98bac6c87e93cbcc76f2a85724cb687a6ff0eba3
dalton-reis <dalton.reis@gmail.com>
2023-09-25 19:47:25 -0300

    .

--- Unidade4/src/CaixaAreia.java
+++ Unidade4/src/CaixaAreia.java
@@ -5,6 +5,14 @@ public class CaixaAreia {
 
 		Scanner teclado = new Scanner(System.in);
 
+		int numero = 10;
+
+		numero = numero / 200;
+		numero /= 200;
+		numero++;
+		System.out.println(numero);
+
+
 		teclado.close();
 
 	}
