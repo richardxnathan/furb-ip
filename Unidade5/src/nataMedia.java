0149f9bca90d2ee9f100a43181eb5f3a1e8b4562
dalton-reis <dalton.reis@gmail.com>
2023-10-16 21:10:11 -0300

    .

---
+++ Unidade5/src/nataMedia.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class nataMedia {
+  public static void main(String[] args) {
+    Scanner tec = new Scanner(System.in);
+    double nota, somaNota, media;
+    final int qtdALunos = 200;
+    final int qtdNotas = 30;
+
+    for (int i = 1; i <= qtdALunos; i++) {
+      somaNota = 0;
+      System.out.println(" __ ALuno: " + i);
+      for (int j = 1; j <= qtdNotas; j++) {
+        System.out.print("nota["+ i + "]: ");
+        nota = tec.nextDouble();
+        somaNota += nota;
+      }
+      media = somaNota / qtdNotas;
+      System.out.println("Média: " + media);
+    }
+
+    tec.close();
+  }}
