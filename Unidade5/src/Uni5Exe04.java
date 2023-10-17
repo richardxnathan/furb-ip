274eae6a91920b929326a255ce59d2615ec8d31e
dalton-reis <dalton.reis@gmail.com>
2023-10-17 10:28:22 -0300

    .

--- Unidade5/src/Uni5Exe04.java
+++ Unidade5/src/Uni5Exe04.java
@@ -4,24 +4,13 @@ public class Uni5Exe04 {
     double denominador = 2;
     double acumulador = 2;
     double soma = 0;
-    int somaNum = 0;
-    int somaDen = 0;
     
     for (int i = 1; i <= 20; i++) {
       soma = soma + ( numerador / denominador);
-
-      //numerador
-      // System.out.println("numerador: " + numerador);
-      // somaNum += numerador;
       numerador += 2;
-      
-      //denominador
-      // System.out.println("denominador: " + denominador);
-      // somaDen += denominador;
       acumulador += 2;
       denominador += acumulador;
     }
-    // System.out.println(somaDen);
     System.out.println("Soma: " + soma);
   }
 }
